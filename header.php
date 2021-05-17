9749b65c5b0f92cfd66789f3388b931969341cf3
yayar <ya.yarochkin@yandex.ru>
2021-05-17 20:15:21 +0300

    galery articles pages

---
+++ header.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">VA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">BIO</a>
+                <a class="nav-item nav-link" href="#">PROJECT</a>
+                <a class="nav-item nav-link " href="#">EVENT</a>
+                <a class="nav-item nav-link " href="#">LOCATION</a>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ js/main.js
@@ -0,0 +1,49 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        } else {
+            change.target.classList.remove('element-show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+;(function () {
+    var throttle = function (type, name, obj) {
+        var obj = obj || window;
+        var running = false;
+        var func = function () {
+            if (running) {
+                return;
+            }
+            running = true;
+            requestAnimationFrame(function () {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+    throttle("scroll", "optimizedScroll");
+})();
+var star = document.getElementById('logo');
+
+window.addEventListener("optimizedScroll", function () {
+    star.style.transform = "rotate(" + window.pageYOffset / 10 + "10deg)"
+
+});
+let menuBtn = document.querySelector('.yaro_menu_btn');
+let menu = document.querySelector('.menu')
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
